c418a0b9d8934d9235a212a90ea8330d392b0ca5
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-07-02 15:27:14 +0600

    mongoose done'

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 // const mongoose = require('mongoose') ;
 // const Playlist = require("./index") ;
 
@@ -7,7 +6,7 @@
 
 //         const result = await Playlist.find();
 //         console.log(result);
-        
+
 //     } catch (error) {
 //         console.log(error);
 //     }
@@ -15,52 +14,53 @@
 
 // findata()
 
-const Jsplaylist = require("./schema");
 
-
-const newcode = async  () =>{
+const newcode = async () => {
     try {
-        
-        const Reactjs = new Jsplaylist({
-            name : "Reactjs",
-            type : "Front-End",
-            videos : 40,
-            author : "Morsalin",
-            email : "morsalin048@gmail.com"
 
-        })
+        // const Reactjs = new Jsplaylist({
+        //     name : "Reactjs",
+        //     type : "Front-End",
+        //     videos : 40,
+        //     author : "Morsalin",
+        //     email : "morsalin048@gmail.com"
 
-        const Nodejs = new Jsplaylist({
-            name : "Nodejs",
-            type : "Back-End",
-            videos : 30,
-            author : "Morsalin",
-            email : "morsalin043@gmail.com"
+        // })
 
-        })
+        // const Nodejs = new Jsplaylist({
+        //     name : "Nodejs",
+        //     type : "Back-End",
+        //     videos : 30,
+        //     author : "Morsalin",
+        //     email : "morsalin043@gmail.com"
 
-        const MongoDB = new Jsplaylist({
-            name : "MongoDB",
-            type : "Database",
-            videos : 60,
-            author : "Morsalin",
-            email : "kausar042@gmail.com"
+        // })
 
-        })
+        // const MongoDB = new Jsplaylist({
+        //     name : "MongoDB",
+        //     type : "Database",
+        //     videos : 60,
+        //     author : "Morsalin",
+        //     email : "kausar042@gmail.com"
 
-        const ExpressJs = new Jsplaylist({
-            name : "ExpressJs",
-            type : "Back-End",
-            videos : 50,
-            author : "Morsalin",
-            email : "kausar043@gmail.com"
+        // })
 
-        })
+        // const ExpressJs = new Jsplaylist({
+        //     name : "ExpressJs",
+        //     type : "Back-End",
+        //     videos : 50,
+        //     author : "Morsalin",
+        //     email : "kausar043@gmail.com"
+
+        // })
 
 
 
-        const result =await Jsplaylist.insertMany([Reactjs,Nodejs,MongoDB,ExpressJs]);
-        console.log(result);
+        // const result =await Jsplaylist.insertMany([Reactjs,Nodejs,MongoDB,ExpressJs]);
+        // console.log(result);
+
+        const result = await Jsplaylist.find();
+        console.log(result)
 
     } catch (error) {
         console.log(error);
